f43540519aafa7ddfc7ab32703fcb6ed856c2ed1
Danny Warren <danny@theforbiddenarts.com>
2022-03-25 00:11:05 -0400

    Ccode submit

--- HomeworkForLoops/Program.cs
+++ HomeworkForLoops/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Please enter a list of first names. seperate each name with a comma(,) and do not use any spaces( ):");
+
+string listEntry = Console.ReadLine();
+
+string[] names = listEntry.Split('\u002C');
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine($"Hello {names[i]}");
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 HomeworkForLoopsApp
+
+Ask the user for a comma-seperated list of first names (no spaces).
+Split the string into a string array. Loop through the array and print
+"Hello <name>" to the Console for each person.
